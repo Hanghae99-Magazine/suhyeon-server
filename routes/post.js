b68b158b85f5f322adc15980d97391d109894ae2
lshyun955 <lshyun955@gmail.com>
2022-02-23 04:51:47 +0900

    초기 프로젝트 세팅

--- routes/post.js
+++ routes/post.js
@@ -5,20 +5,20 @@ const authMiddleware = require("../middlewares/auth-middleware");
 
 const router = express.Router();
 
-// 목록 가져오기
+// 목록 가져오기 (인증X)
 router.get("/", async (req, res) => {});
 
-// 게시글 추가
+// 게시글 추가 (인증O)
 router.post("/", async (req, res) => {});
 
-// 게시글 좋아요 및 좋아요 취소
+// 게시글 좋아요 및 좋아요 취소 (인증O)
 router.put("/:postId/like", async (req, res) => {});
 
-// 게시글 상세조회
+// 게시글 조회 (인증X)
 router.get("/:postId", async (req, res) => {});
 
-// 게시글 삭제
+// 게시글 삭제 (인증O)
 router.delete("/:postId", async (req, res) => {});
 
-// 게시글 수정
+// 게시글 수정 (인증O)
 router.put("/:postId", async (req, res) => {});

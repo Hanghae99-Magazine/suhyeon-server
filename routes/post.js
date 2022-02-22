092109cd309b783b7c06662dd6a081484cc65779
lshyun955 <lshyun955@gmail.com>
2022-02-23 07:42:48 +0900

    게시글 등록 기능(이미지 S3 업로드 구현중)

--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { Op } = require("sequelize");
+const cors = require("cors");
 const { user, board, likes, comment } = require("../models");
 const authMiddleware = require("../middlewares/auth-middleware");
+const upload = require("../utils/s3");
 
 const router = express.Router();
 
@@ -12,9 +14,26 @@ router.get("/", async (req, res) => {
 });
 
 // 게시글 추가 (인증O)
-router.post("/", authMiddleware, async (req, res) => {
-  const { user_id, img_position, post_img, post_content } = req.body;
-});
+router.post(
+  "/",
+  authMiddleware,
+  upload.single("imageFile"),
+  async (req, res) => {
+    const { user_id, post_content } = req.body;
+    const { location } = req.file;
+
+    if (!post_content && !location) {
+      res.status(400).send({ msg: "사진 또는 내용을 입력해주세요!" });
+    }
+
+    await board.create({
+      userId: user_id,
+      content: post_content,
+      img: location,
+    });
+    res.status(201).send({ msg: "게시물이 등록되었습니다!" });
+  }
+);
 
 // 게시글 좋아요 및 좋아요 취소 (인증O)
 router.put("/:postId/like", authMiddleware, async (req, res) => {});
---
+++ utils/s3.js
@@ -0,0 +1,27 @@
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const AWS = require("aws-sdk");
+const path = require("path");
+require("dotenv").config();
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.S3_KEYID,
+  secretAccessKey: process.env.S3_PRIVATE_KEY,
+  region: process.env.REGION,
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.BUCKET_NAME,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: "public-read",
+    key: (req, res, cb) => {
+      let extension = path.extname(file.originalname);
+      cb(null, `${Date.now()}_${file.originalname}`);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+module.exports = upload;
